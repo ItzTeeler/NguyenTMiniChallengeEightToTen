21cd04795d8fc70d1c66b17de14a2012773c2ba4
itzteeler <tylernguyen518@gmail.com>
2024-01-11 13:39:54 -0800

    Publish Branch

--- Controllers/GuessItController.cs
+++ Controllers/GuessItController.cs
@@ -14,19 +14,19 @@ namespace NguyenTMiniChallengeEightToTen.Controllers;
         _guessItService = guessItService;
     }
         [HttpGet]
-        [Route("GuessItEasy/{input1}")]
+        [Route("GuessItEasy(1-10)/{input1}")]
         public string GuessItEasy (string input1){
             return _guessItService.GuessItEasy(input1);
         }
 
         [HttpGet]
-        [Route("GuessItMedium/{input1}")]
+        [Route("GuessItMedium(1-50)/{input1}")]
         public string GuessItMedium (string input1){
             return _guessItService.GuessItMedium(input1);
         }
 
         [HttpGet]
-        [Route("GuessItHard/{input1}")]
+        [Route("GuessItHard(1-100)/{input1}")]
         public string GuessItHard (string input1){
             return _guessItService.GuessItHard(input1);
         }
